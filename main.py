109e0cf6f12167ec296a251b67233788c0f68897
akimasa-l <liumingzheng8@outlook.com>
2021-07-03 19:43:59 +0900

    完成したかも！？

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+import random
 
 driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver")
-url="https://docs.google.com/forms/d/e/1FAIpQLSco7LzMebv1ctlOFOvWOZEkd7XAzeNt85bfXawibNDr52ZBNg/viewform"
+url=f"https://docs.google.com/forms/d/e/1FAIpQLSco7LzMebv1ctlOFOvWOZEkd7XAzeNt85bfXawibNDr52ZBNg/viewform?usp=pp_url&entry.704563200=なしおたかし&entry.925303296={random.randint(360,365)/10}"
+
 driver.get(url)
 username="61229liu@seiko.ac.jp"
 with open("./password.txt") as f:
@@ -18,8 +20,9 @@ wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"inp
 password_element=driver.find_element_by_css_selector("input[name=\"password\"]")
 password_element.send_keys(password)
 password_element.send_keys(Keys.ENTER)
-wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"span.freebirdFormviewerViewHeaderEmailAddress")))
-element=driver.find_element_by_css_selector("input")
-element.send_keys(Keys.TAB)
-element.send_keys(Keys.TAB)
-element.send_keys("なしおたかし")
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"span.freebirdFormviewerViewHeaderEmailAddress")))#読み込まれるまで待つ
+send_button=driver.find_element_by_css_selector("div[role=\"button\"]")
+send_button.click()
+
+driver.close
+driver.quit

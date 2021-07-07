7690122889e61d5068e5e482b007d6a347225446
akimasa-l <liumingzheng8@outlook.com>
2021-07-07 18:15:21 +0900

    完成した！！！！！！！！！！！！

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 import random
 
-driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver")
-url=f"https://docs.google.com/forms/d/e/1FAIpQLSco7LzMebv1ctlOFOvWOZEkd7XAzeNt85bfXawibNDr52ZBNg/viewform?usp=pp_url&entry.704563200=なしおたかし&entry.925303296={random.randint(360,365)/10}"
+driver = webdriver.Chrome(
+    executable_path="/usr/lib/chromium-browser/chromedriver")
+
+with open("./name.txt") as f:
+    name = f.read()
+
+url = f"https://docs.google.com/forms/d/e/1FAIpQLSco7LzMebv1ctlOFOvWOZEkd7XAzeNt85bfXawibNDr52ZBNg/viewform?usp=pp_url&entry.704563200={name}&entry.925303296={random.randint(360,365)/10}"
 
 driver.get(url)
-username="61229liu@seiko.ac.jp"
+username = "61229liu@seiko.ac.jp"
 with open("./password.txt") as f:
     password = f.read()
-email=driver.find_element_by_css_selector("#identifierId")
+email = driver.find_element_by_css_selector("#identifierId")
 email.send_keys(username)
 email.send_keys(Keys.ENTER)
-wait=WebDriverWait(driver,10)
-wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"input[name=\"password\"]")))
-password_element=driver.find_element_by_css_selector("input[name=\"password\"]")
+wait = driver.WebDriverWait(10)#waitインスタンスを作る
+wait.until(expected_conditions.presence_of_element_located(
+    (By.CSS_SELECTOR, "input[name=\"password\"]")))  # パスワードの画面が読み込まれるまで待つ
+password_element = driver.find_element_by_css_selector(
+    "input[name=\"password\"]")
 password_element.send_keys(password)
 password_element.send_keys(Keys.ENTER)
-wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"span.freebirdFormviewerViewHeaderEmailAddress")))#読み込まれるまで待つ
-send_button=driver.find_element_by_css_selector("div[role=\"button\"]")
+wait.until(expected_conditions.presence_of_element_located(
+    (By.CSS_SELECTOR, "span.freebirdFormviewerViewHeaderEmailAddress")))  # フォーム画面が読み込まれるまで待つ
+send_button = driver.find_element_by_css_selector("div[role=\"button\"]")
 send_button.click()
-wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,"div.freebirdFormviewerViewResponseConfirmationMessage")))
+wait.until(expected_conditions.presence_of_element_located(
+    (By.CSS_SELECTOR, "div.freebirdFormviewerViewResponseConfirmationMessage")))  # 終了画面が読み込まれるまで待つ
 
 driver.close()
 driver.quit()
